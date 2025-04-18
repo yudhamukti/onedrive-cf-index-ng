8d0d39567de8294a4409c273194af2c5ac59b283
Yudha Dwi Mukti <54300906+yudhamukti@users.noreply.github.com>
2025-04-18 08:20:48 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   title: "OtakuSubs OneDrive",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
-  baseDirectory: process.env.BASE_DIRECTORY || '/6AA31E843E32AC4C',
+  baseDirectory: process.env.BASE_DIRECTORY || '/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
